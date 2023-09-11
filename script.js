d757f7b2b58259c6da8e18fb9d9646a0ef4907f7
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2023-09-11 11:24:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function threeSum(arr, target) {
-// write your code here
-  
-}
+function threeSumClosest(S, target) {
+  S.sort((a, b) => a - b);
 
-module.exports = threeSum;
+  let closestSum = Infinity;
+  let closestDiff = Infinity;
+
+  for (let i = 0; i < S.length - 2; i++) {
+    let left = i + 1;
+    let right = S.length - 1;
+
+    while (left < right) {
+      const sum = S[i] + S[left] + S[right];
+      const diff = Math.abs(target - sum);
+
+      if (diff < closestDiff) {
+        closestSum = sum;
+        closestDiff = diff;
+      }
+
+      if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return closestSum;
+}
