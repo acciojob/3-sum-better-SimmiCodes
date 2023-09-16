edb4cc7e9e98f44bbfb50f71304e16aceaee14b7
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2023-09-16 23:49:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-function findClosestSum(nums, target) {
- nums.sort((a, b) => a - b);
- let closestSum = Infinity;
-
- for (let i = 0; i < nums.length - 2; i++) {
-    let left = i + 1;
-    let right = nums.length - 1;
-
-    while (left < right) {
-      let sum = nums[i] + nums[left] + nums[right];
-
-      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-        closestSum = sum;
-      }
-
-      if (sum <= target) {
-        left++;
-      } else {
-        right--;
-      }
+function threeSumClosest(nums, target) {
+    nums.sort((a, b) => a - b); 
+    let closestSum = Infinity;
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+        
+        while (left < right) {
+            const currentSum = nums[i] + nums[left] + nums[right];
+            
+            if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                closestSum = currentSum;
+            }
+            
+            if (currentSum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
     }
- }
-
- return closestSum;
+    
+    return closestSum;
 }
 
-let nums = [1, 2, 3, 4, 5];
-let target = 10;
-console.log(findClosestSum(nums, target));
