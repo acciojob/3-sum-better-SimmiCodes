2277c6740dcfa88c5eb3d1171873773e8d5b4780
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2023-09-20 11:47:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function threeSum(nums, target) {
     nums.sort((a, b) => a - b);
     let result = nums[0] + nums[1] + nums[2];
@@ -7,6 +6,7 @@ function threeSum(nums, target) {
         let k = nums.length - 1;
         while (j < k) {
             let sum = nums[i] + nums[j] + nums[k];
+            console.log(`i: ${i}, j: ${j}, k: ${k}, sum: ${sum}, result: ${result}`);
             if (Math.abs(target - sum) < Math.abs(target - result)) {
                 result = sum;
             }
